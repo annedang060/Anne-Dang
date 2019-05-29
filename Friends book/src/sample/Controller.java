135e66352893c116c40b845c5a58eb87dc17ffc8
annedang060 <43803827+annedang060@users.noreply.github.com>
2019-05-28 23:09:56 -0700

    Add files via upload

---
+++ Friends book/src/sample/Controller.java
@@ -0,0 +1,58 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+
+public class Controller {
+
+    public TextField textGetName;
+    public TextField textGetSchool;
+    public TextField textGetAge;
+    public ListView<Product> productList = new ListView<Product>();
+    public Label lblName;
+    public Label lblSchool;
+    public Label lblAge;
+    public Button btnRemove;
+
+
+    public void addFriend(ActionEvent actionEvent) {
+        String name = textGetName.getText();
+        String school = textGetSchool.getText();
+        int age = Integer.parseInt(textGetAge.getText());
+
+        Product temp = new Product(name, school, age);
+        productList.getItems().add(temp);
+        textGetName.clear();
+        textGetSchool.clear();
+        textGetAge.clear();
+    }
+
+   public void displayFriend(MouseEvent mouseEvent){
+       Product temp;
+       temp = productList.getSelectionModel().getSelectedItem();
+       lblName.setText(temp.name);
+       lblSchool.setText(temp.school);
+       lblAge.setText(Integer.toString(temp.getAge()));
+
+   }
+
+   public void removeFriend(ActionEvent actionEvent){
+       String name = textGetName.getText();
+       String school = textGetSchool.getText();
+       int age = Integer.parseInt(textGetAge.getText());
+
+       Product temp = new Product(name, school, age);
+       productList.getItems().remove(temp);
+
+
+
+   }
+
+
+
+}
---
+++ Friends book/src/sample/Product.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class Product {
+    public String name;
+    public String school;
+     private int age;
+
+
+     Product(String n, String school, int age){
+         name = n;
+         this.school = school;
+         this.age = age;
+
+
+     }
+     //getters and setters
+
+       public int getAge(){
+         return age;
+       }
+
+       public String toString(){
+         return name;
+       }
+}
